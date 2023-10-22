f90f414dbaffbe6c80c18658767ce524f5c1875b
andrewbantly <andrewbantly@gmail.com>
2023-10-22 16:01:03 -0700

    themes

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,83 +1,64 @@
 import React, { useState } from 'react';
-import { Box, IconButton, useBreakpointValue, Heading } from '@chakra-ui/react';
-import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-// Slider settings
-const settings = {
-  dots: true,
-  arrows: false,
-  fade: true,
-  infinite: true,
-  autoplay: true,
-  speed: 500,
-  autoplaySpeed: 5000,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-};
+import {
+  Box,
+  Grid,
+  Image,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  Heading,
+} from '@chakra-ui/react';
 
 export function PastProjects() {
-  const [slider, setSlider] = useState(null);
-  const top = useBreakpointValue({ base: '90%', md: '50%' }) || '90%'; // Fallback value if undefined
-  const side = useBreakpointValue({ base: '30%', md: '10px' }) || '30%'; // Fallback value if undefined
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedImage, setSelectedImage] = useState('');
 
-  // Images for the carousel
-  const projects = [
-    '/photos/projects-image-01.jpg',
-    '/photos/projects-image-02.jpg',
-    '/photos/projects-image-03.jpg',
-    '/photos/projects-image-04.jpg',
-    '/photos/projects-image-05.jpg',
-  ];
+ // Images for the carousel
+ const projects = [
+  '/photos/projects-image-01.jpg',
+  '/photos/projects-image-02.jpg',
+  '/photos/projects-image-03.jpg',
+  '/photos/projects-image-04.jpg',
+  '/photos/projects-image-05.jpg',
+  '/photos/projects-image-06.jpg',
+  '/photos/projects-image-07.jpg',
+  '/photos/projects-image-08.jpg',
+  '/photos/projects-image-09.jpg',
+];
+
+  const handleImageClick = (url) => {
+    setSelectedImage(url);
+    onOpen();
+  };
 
   return (
     <Box id="projects">
       <Heading as="h2" size="xl" textAlign="center" m={6}>
         Our Projects
       </Heading>
-      <Box position={'relative'} height={'600px'} width={'full'} overflow={'hidden'}>
-        <IconButton
-          aria-label="left-arrow"
-          icon={<BiLeftArrowAlt />}
-          colorScheme="messenger"
-          borderRadius="full"
-          position="absolute"
-          left={side}
-          top={top}
-          transform={'translate(0%, -50%)'}
-          zIndex={2}
-          onClick={() => slider?.slickPrev()}
-        />
-        {/* Right arrow button */}
-        <IconButton
-          aria-label="right-arrow"
-          icon={<BiRightArrowAlt />}
-          colorScheme="messenger"
-          borderRadius="full"
-          position="absolute"
-          right={side}
-          top={top}
-          transform={'translate(0%, -50%)'}
-          zIndex={2}
-          onClick={() => slider?.slickNext()}
-        />
-        <Slider {...settings} ref={setSlider}>
-          {projects.map((url, index) => (
-            <Box
-              key={index}
-              height={'600px'}
-              width={'100%'}
-              position="relative"
-              backgroundPosition="center"
-              backgroundRepeat="no-repeat"
-              backgroundSize="contain"
-              backgroundImage={`url(${url})`}
+      <Grid templateColumns={{ sm: "repeat(1, 1fr)", md: "repeat(3, 1fr)" }} gap={6}>
+        {projects.map((url, index) => (
+          <Box key={index} as="button" onClick={() => handleImageClick(url)}>
+            <Image
+              src={url}
+              alt={`Project ${index + 1}`}
+              width="100%"
+              height="100%"
+              objectFit="cover"
             />
-          ))}
-        </Slider>
-      </Box>
+          </Box>
+        ))}
+      </Grid>
+
+      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <Image src={selectedImage} alt="Selected project" width="100%"/>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -5,6 +5,32 @@ const theme = extendTheme({
     heading: "'Rajdhani', sans-serif",
     body: "'Rajdhani', sans-serif",
   },
+  colors: {
+    brand: {
+      900: "#1a365d",  // Replace with your actual color code
+      800: "#153e75",  // Replace with your actual color code
+      700: "#2a69ac",  // Replace with your actual color code
+    },
+    // Add more color ranges or individual colors as needed
+    background: "#f0f0f0", // Replace with your actual color code for background
+  },
+  // You can also set global styles
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        bg: "background", // use the color key in `theme.colors`
+        color: "black",  // Replace with your actual color code for text color
+      },
+      // styles for the `a`
+      a: {
+        color: "brand.700", // use the color key in `theme.colors`
+        _hover: {
+          textDecoration: "none",
+        },
+      },
+    },
+  },
 });
 
 export default theme;
