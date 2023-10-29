cd95c96a5b698ad8b0085566ecb7815c26d4d0fe
andrewbantly <andrewbantly@gmail.com>
2023-10-29 10:35:30 -0700

    theme description

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,3 +1,31 @@
+/**
+ * Custom Theme Configuration for Chakra UI
+ *
+ * Overview:
+ * This custom theme for Chakra UI extends the default theme with specific breakpoints, font settings, color schemes, and global styles to provide a consistent and tailored aesthetic across the application.
+ *
+ * Breakpoints:
+ * - 'sm': 360px for small mobile devices.
+ * - 'md': 768px for tablets and small desktops.
+ * - 'lg': 1024px for large tablets and medium desktops.
+ * - 'xl': 1280px for large desktops.
+ * - '2xl': 1536px for extra-large desktops and monitors.
+ * These breakpoints ensure responsive design and fluidity for different screen sizes.
+ *
+ * Fonts:
+ * The theme specifies 'Istok Web' and 'Rajdhani' as the primary font families for headings and body text, enhancing the textual clarity and overall readability.
+ *
+ * Colors:
+ * - 'brand': A series of color shades (900, 800, 700) to provide a cohesive branding palette.
+ * - 'background': A light background color to maintain visual comfort and content legibility.
+ * Additional colors can be added to the palette to suit specific design requirements.
+ *
+ * Global Styles:
+ * - The 'body' style sets a consistent background and text color across the website.
+ * - The 'a' (anchor) style defines the default and hover states for all hyperlink elements,
+ * enhancing the interactive experience without underlines on hover to keep a clean visual.
+ */
+
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
