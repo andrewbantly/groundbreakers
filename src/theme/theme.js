be61606aa96f676eb3f0cd51e5a61b05ec03d7c6
andrewbantly <andrewbantly@gmail.com>
2023-10-29 10:36:04 -0700

    cleanup

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -42,24 +42,20 @@ const theme = extendTheme({
   },
   colors: {
     brand: {
-      900: "#1a365d",  // Replace with your actual color code
-      800: "#153e75",  // Replace with your actual color code
-      700: "#2a69ac",  // Replace with your actual color code
+      900: "#1a365d",
+      800: "#153e75",
+      700: "#2a69ac",
     },
-    // Add more color ranges or individual colors as needed
-    background: "#f0f0f0", // Replace with your actual color code for background
+    background: "#f0f0f0",
   },
-  // You can also set global styles
   styles: {
     global: {
-      // styles for the `body`
       body: {
-        bg: "background", // use the color key in `theme.colors`
-        color: "black",  // Replace with your actual color code for text color
+        bg: "background",
+        color: "black",
       },
-      // styles for the `a`
       a: {
-        color: "brand.700", // use the color key in `theme.colors`
+        color: "brand.700",
         _hover: {
           textDecoration: "none",
         },
