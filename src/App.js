0fcdacadbd3fdbfc396e113d7a537bee985e23bb
andrewbantly <andrewbantly@gmail.com>
2023-10-29 10:38:33 -0700

    cleanup

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@
  * - ChakraProvider: This component from Chakra UI takes a 'theme' prop to provide a consistent styling baseline throughout the application.
  * - BrowserRouter: A Router component from React Router that uses the HTML5 history API to keep UI in sync with the URL.
  * - Header: A custom header component that provides navigation functionality for the application.
- * - Routes and Route: React Router components that declare possible routes in the application.
- *   Currently, only the root path '/' is defined, which renders the Main component.
+ * - Routes and Route: React Router components that declare possible routes in the application. Currently, only the root path '/' is defined, which renders the Main component.
  * - Main: This component serves as the landing page for the application, displayed when the user navigates to the root URL.
  *
  * Usage:
